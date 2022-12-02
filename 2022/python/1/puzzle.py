f67257622ecb80135f37f0a947fdceb7ffc33296
Jeroen Oldenburger <jeroen@socradev.com>
2022-12-02 16:27:59 +0100

    Solve 2022 day 1 part 2

--- 2022/python/1/puzzle.py
+++ 2022/python/1/puzzle.py
@@ -1,19 +1,3 @@
-def solve():
-    lines = open("input.txt").read().splitlines()
-    elves = []
-    item_calories = []
-    for line in lines:
-        if not line:
-            elves.append(Elf(inventory=Inventory(item_calories=item_calories)))
-            item_calories = []
-        else:
-            item_calories.append(int(line))
-    if item_calories:
-        elves.append(Elf(inventory=Inventory(item_calories=item_calories)))
-    elf_with_largest_inventory = max(elves, key=lambda elf: elf.inventory_calorie_sum())
-    return elf_with_largest_inventory.inventory_calorie_sum()
-
-
 class Inventory:
     item_calories: [int]
 
@@ -34,5 +18,32 @@ class Elf:
         return self.inventory.calorie_sum()
 
 
+def get_elves() -> [Elf]:
+    lines = open("input.txt").read().splitlines()
+    global elves
+    elves = []
+    item_calories = []
+    for line in lines:
+        if not line:
+            elves.append(Elf(inventory=Inventory(item_calories=item_calories)))
+            item_calories = []
+        else:
+            item_calories.append(int(line))
+    if item_calories:
+        elves.append(Elf(inventory=Inventory(item_calories=item_calories)))
+    return elves
+
+
+def solve_part1() -> int:
+    elf_with_largest_inventory = max(elves, key=lambda elf: elf.inventory_calorie_sum())
+    return elf_with_largest_inventory.inventory_calorie_sum()
+
+
+def solve_part2() -> int:
+    elves.sort(key=lambda elf: elf.inventory_calorie_sum(), reverse=True)
+    return sum([elf.inventory_calorie_sum() for elf in elves[0:3]])
+
+
 if __name__ == '__main__':
-    print(solve())
+    get_elves()
+    print(solve_part2())
