90a3171a5b92738cdad83fc83bbd713d716caa51
Jeroen Oldenburger <jeroen@socradev.com>
2022-12-02 16:14:35 +0100

    Solve 2022 day 1 part 1

---
+++ 2022/python/1/puzzle.py
@@ -0,0 +1,38 @@
+def solve():
+    lines = open("input.txt").read().splitlines()
+    elves = []
+    item_calories = []
+    for line in lines:
+        if not line:
+            elves.append(Elf(inventory=Inventory(item_calories=item_calories)))
+            item_calories = []
+        else:
+            item_calories.append(int(line))
+    if item_calories:
+        elves.append(Elf(inventory=Inventory(item_calories=item_calories)))
+    elf_with_largest_inventory = max(elves, key=lambda elf: elf.inventory_calorie_sum())
+    return elf_with_largest_inventory.inventory_calorie_sum()
+
+
+class Inventory:
+    item_calories: [int]
+
+    def __init__(self, item_calories: [int]):
+        self.item_calories = item_calories
+
+    def calorie_sum(self) -> int:
+        return sum(x for x in self.item_calories)
+
+
+class Elf:
+    inventory: Inventory
+
+    def __init__(self, inventory: Inventory):
+        self.inventory = inventory
+
+    def inventory_calorie_sum(self) -> int:
+        return self.inventory.calorie_sum()
+
+
+if __name__ == '__main__':
+    print(solve())
