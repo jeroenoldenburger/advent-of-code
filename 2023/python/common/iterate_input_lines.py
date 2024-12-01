bd2e08833823f4593f2a6ee13280854db75395d6
Jeroen Oldenburger <jeroen@socradev.com>
2024-12-01 21:14:56 +0100

    changes of last year

--- 2023/python/common/iterate_input_lines.py
+++ 2023/python/common/iterate_input_lines.py
@@ -2,8 +2,8 @@
 from typing import Iterator
 
 
-def iterate_input_lines(case) -> Iterator[str]:
+def iterate_input_lines(case, ignore_commented_lines=True) -> Iterator[str]:
     with open(case) as input_file:
         for line in input_file:
-            if not line.startswith("#"):
+            if not ignore_commented_lines or not line.startswith("#"):
                 yield line.strip()
