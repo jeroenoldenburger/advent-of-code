93b62df0f6468c36fb6e1b0e17b3262586589bfb
Jeroen Oldenburger <jeroen@socradev.com>
2024-12-05 07:10:34 +0100

    WIP day04, not finished

--- 2024/day04/solution.py
+++ 2024/day04/solution.py
@@ -1,17 +1,16 @@
+import re
+
 import numpy as np
 import polars as pl
 
 
 def __read_fwf(input_file_path):
     # see https://medium.com/@arkimetrix.analytics/part-7-python-polars-working-with-fixed-width-files-edge-case-9a7b1e710b98
-    # for the initial idea
-    # changes TODO:
-    # - detect num of columns by the first line
-    # - don't name columns as we don't need them
     df = pl.read_csv(input_file_path, has_header=False, skip_rows=0, new_columns=["full_str"])
 
-    column_names = [f"{i}" for i in range(140)]
-    widths = [1] * 140
+    file_width = 140  # TODO this could be read from the first line of the file
+    column_names = [f"{i}" for i in range(file_width)]
+    widths = [1] * file_width
 
     # Calculate slice values from widths.
     slice_tuples = []
@@ -29,55 +28,107 @@ def __read_fwf(input_file_path):
     ).drop("full_str")
 
 
+def __find_in_source(source, term):
+    # left to right
+    l_to_r = source.count(term)
+    # right to left
+    r_to_l = source[::-1].count(term)
+    print(f"l_to_r: {l_to_r} - r_to_l: {r_to_l}")
+    return l_to_r + r_to_l
+
+
 def part1(input_file_path):
     df = __read_fwf(input_file_path)
     matrix = df.to_numpy()
     num_of_rows = matrix.shape[0]
     num_of_xmas = 0
-
     word_to_find = "XMAS"
-
     # check matrix horizontally
-    for line in matrix:
-        line_as_string = ''.join(line)
-        # left to right
-        l_to_r = line_as_string.count("XMAS")
-        num_of_xmas += l_to_r
-        # right to left
-        r_to_l = line_as_string[::-1].count("XMAS")
-        num_of_xmas += r_to_l
-        print(f"row - l_to_r: {l_to_r} - r_to_l: {r_to_l} - num_of_xmas: {num_of_xmas}")
+    for row in matrix:
+        print("row")
+        num_of_xmas += __find_in_source(''.join(row), word_to_find)
     # check matrix "vertically"
     matrix2 = np.transpose(matrix)
-    for line in matrix2:
-        line_as_string = ''.join(line)
-        # top to bottom
-        l_to_r = line_as_string.count("XMAS")
-        num_of_xmas += l_to_r
-        # bottom to top
-        r_to_l = line_as_string[::-1].count("XMAS")
-        num_of_xmas += r_to_l
-        print(f"column - t_to_b: {l_to_r} - b_to_t: {r_to_l} - num_of_xmas: {num_of_xmas}")
-
+    for column in matrix2:
+        print("column")
+        num_of_xmas += __find_in_source(''.join(column), word_to_find)
     # get left-right diagonals
     list_ = [''.join(np.diag(matrix, k=i).tolist()) for i in range(-num_of_rows + 1, num_of_rows)]
     for diag in list_:
-        # bottom to top
-        num_of_xmas += diag.count("XMAS")
-        # top to bottom
-        num_of_xmas += diag[::-1].count("XMAS")
+        print("diag l to r")
+        num_of_xmas += __find_in_source(diag, word_to_find)
     # get right-left reverse diagonals
     matrix3 = np.flipud(matrix)
     list_ = [''.join(np.diag(matrix3, k=i).tolist()) for i in range(-num_of_rows + 1, num_of_rows)]
     for diag in list_:
-        # bottom to top
-        num_of_xmas += diag.count("XMAS")
-        # top to bottom
-        num_of_xmas += diag[::-1].count("XMAS")
+        print("diag r to l")
+        num_of_xmas += __find_in_source(diag, word_to_find)
 
     print(num_of_xmas)
 
 
+def __find_positions(source, term):
+    return [m.start() for m in re.finditer(f'(?={term})', source)]
+
+
+def part2(input_file_path):
+    df = __read_fwf(input_file_path)
+    matrix = df.to_numpy()
+    num_of_rows = matrix.shape[0]
+    word_to_find = "MAS"
+    # get right-left diagonals, starting from bottom
+    for i in range(-num_of_rows + 1, num_of_rows):
+        diag = ''.join(np.diag(matrix, k=i).tolist())
+        print("######")
+        print(diag)
+        # find matches reading diagonal from left to right
+        starts = __find_positions(diag, term=word_to_find)
+        if starts:
+            # find middles
+            if i <= 0:
+                # find zero base row and column indexes for the middle of MAS
+                row = abs(i)
+                # print middles
+                for start in starts:
+                    num_of_steps = start + 1
+                    print(f"row: {row+num_of_steps}, column: {num_of_steps}")
+            else:
+                above_zero_rows = i
+                row = -above_zero_rows
+                # print middles
+                for start in starts:
+                    num_of_steps = len(diag) - start + 1
+                    print(f"az row: {row+num_of_steps}, column: {num_of_steps}")
+
+        # find matches reading diagonal from right to left
+        print("-------")
+        starts = __find_positions(diag[::-1], term=word_to_find)
+        if starts:
+            # find middles
+            if i <= 0:
+                # find zero base row and column indexes for the middle of MAS
+                row = abs(i)
+                # print middles
+                for start in starts:
+                    num_of_steps = len(diag) - start - 1
+                    print(f"row: {row + num_of_steps}, column: {num_of_steps}")
+
+        # starts += __find_positions(diag[::-1], term=word_to_find)
+
+    # list_ = [''.join(np.diag(matrix, k=i).tolist()) for i in range(-num_of_rows + 1, num_of_rows)]
+    # for diag in list_:
+    #     print("diag l to r")
+    #     num_of_xmas += __find_in_source(diag, word_to_find)
+    # # get right-left reverse diagonals
+    # matrix3 = np.flipud(matrix)
+    # list_ = [''.join(np.diag(matrix3, k=i).tolist()) for i in range(-num_of_rows + 1, num_of_rows)]
+    # for diag in list_:
+    #     print("diag r to l")
+    #     num_of_xmas += __find_in_source(diag, word_to_find)
+    #
+    # print(num_of_xmas)
+
+
 if __name__ == '__main__':
-    part1("input.txt")
+    part2("example.txt")
     exit(0)
